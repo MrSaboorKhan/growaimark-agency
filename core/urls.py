9549982a1fbd9208f89f78c1a780dd964ecdfa29
MrSaboorKhan <saborkhan1026@gmail.com>
2026-01-04 09:25:34 +0800

    fixed views import error in urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-from . import views
-
+from website import views
 urlpatterns = [
-    # 1. Main Home Page ka rasta
     path('', views.home, name='home'),
-
-    # 2. Saare Blogs dikhane ka rasta
     path('blog/', views.blog_list, name='blog_list'),
-
-    # 3. Contact Form submit karne ka rasta (Iska name 'contact_view' hona lazmi hai)
     path('contact-submit/', views.contact_view, name='contact_view'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,33 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Blog, ContactMessage
 
-
-# 1. Home Page View
 def home(request):
     blogs = Blog.objects.all().order_by('-date_posted')[:3]
     return render(request, 'website/home.html', {'blogs': blogs})
 
-
-# 2. Blog List Page View
 def blog_list(request):
     all_blogs = Blog.objects.all().order_by('-date_posted')
     return render(request, 'website/blog_list.html', {'blogs': all_blogs})
 
-
-# 3. Contact Form Submission View
 def contact_view(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-
         ContactMessage.objects.create(
-            name=name,
-            email=email,
-            phone=phone,
-            message=message
+            name=request.POST.get('name'),
+            email=request.POST.get('email'),
+            phone=request.POST.get('phone'),
+            message=request.POST.get('message')
         )
-        return redirect('home')
-
     return redirect('home')
